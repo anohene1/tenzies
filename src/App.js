d641641ba52fc93fe7ba2c9dadfa8054432b9f29
anohene1 <anoheneisaac@gmail.co,m>
2022-01-27 13:47:36 +0000

    roll dice button complete

--- src/App.js
+++ src/App.js
@@ -4,23 +4,28 @@ import Die from "./components/Die";
 import {useState} from "react";
 
 function App() {
-    const [dice, setDice] = useState(allNewDice)
-    const allDice = dice.map((die, index) => <Die key={index} value={die}/>)
+    const [dice, setDice] = useState(allNewDice);
+    const allDice = dice.map((die, index) => <Die key={index} value={die.value}/>);
     
     function allNewDice() {
         let newDice = [];
         for (let i = 1; i <= 10; i++) {
             let randomNumber = Math.floor((Math.random() * 6) + 1);
-            newDice.push(randomNumber);
+            newDice.push({value: randomNumber, isHeld: false});
         }
         return newDice;
     }
 
-  return (
+    function handleRollDice() {
+        setDice(allNewDice());
+    }
+
+    return (
       <main className='container'>
           <Dice>
               {allDice}
           </Dice>
+          <button className='roll-dice' onClick={handleRollDice}>Roll</button>
       </main>
   );
 }
