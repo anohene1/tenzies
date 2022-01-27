792d54191045a61fee3b7db93c2be87637d95c76
anohene1 <anoheneisaac@gmail.co,m>
2022-01-27 15:33:13 +0000

    complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import Dice from "./components/Dice";
 import Die from "./components/Die";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {nanoid} from "nanoid";
+import ReactConfetti from "react-confetti";
 
 function App() {
     const [dice, setDice] = useState(allNewDice);
+    const [tenzies, setTenzies] = useState(false);
+
+    useEffect(function () {
+        const allHeld = dice.every(die => die.isHeld);
+        const firstValue = dice[0].value;
+        const allSameValue = dice.every(die => die.value === firstValue);
+        if (allHeld && allSameValue) {
+            setTenzies(true);
+        }
+    }, [dice]);
+
     const allDice = dice.map(die => <Die
         key={die.id}
         value={die.value}
@@ -26,10 +38,19 @@ function App() {
     }
 
     function handleRollDice() {
-        setDice(oldDice => oldDice.map(oldDie => {
-            let randomNumber = Math.floor((Math.random() * 6) + 1);
-            return oldDie.isHeld ? oldDie : {value: randomNumber, isHeld: false, id: nanoid()}
-        }));
+        if (tenzies) {
+            setDice(allNewDice());
+            setTenzies(false);
+        } else {
+            setDice(oldDice => oldDice.map(oldDie => {
+                let randomNumber = Math.floor((Math.random() * 6) + 1);
+                return oldDie.isHeld ? oldDie : {
+                    value: randomNumber,
+                    isHeld: false,
+                    id: nanoid(),
+                }
+            }));
+        }
     }
 
     function holdDie(id) {
@@ -48,10 +69,15 @@ function App() {
     
     return (
       <main className='container'>
+          {
+              tenzies && <ReactConfetti />
+          }
+          <h1 className='title'>Tenzies</h1>
+          <p className="description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
           <Dice>
               {allDice}
           </Dice>
-          <button className='roll-dice' onClick={handleRollDice}>Roll</button>
+          <button className='roll-dice' onClick={handleRollDice}>{tenzies ? 'New Game': 'Roll'}</button>
       </main>
   );
 }
